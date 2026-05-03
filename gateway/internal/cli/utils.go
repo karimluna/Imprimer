eca50c616f8d8665c804d4896ed547e9aa63ac48
Balor.LC3 <supernova20315@gmail.com>
2026-05-02 19:19:56 -0700

    feat: truncation of long strings

---
+++ gateway/internal/cli/utils.go
@@ -0,0 +1,9 @@
+package cli
+
+// truncate shortens a string to maxLen characters with an ellipsis.
+func truncate(s string, maxLen int) string {
+	if len(s) <= maxLen {
+		return s
+	}
+	return s[:maxLen-3] + "..."
+}
